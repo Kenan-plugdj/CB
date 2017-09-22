3a4b08a9e2a4cf9b9c3c509657a2b102907064cc
stiefandawidow <30799966+stiefandawidow@users.noreply.github.com>
2017-09-22 05:19:25 +0200

    Update Catharsis-Bot.js

--- Catharsis-Bot.js
+++ Catharsis-Bot.js
@@ -2292,7 +2292,45 @@
                     }
                 }
             },
-
+         
+            ctCommand: {
+                command: 'ct',
+                rank: 'user',
+                type: 'startsWith',
+                getcts: function (chat) {
+                    var sho = Math.floor(Math.random() * basicBot.chat.cts.length);
+                    return basicBot.chat.cts[sho];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatct);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (name === "djs") {
+                                return API.sendChat(subChat(basicBot.chat.multict, {namefrom: chat.un, ct: this.getCts()}));
+                            }
+                            else if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nouserct, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfct, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.ct, {nameto: user.username, namefrom: chat.un, ct: this.getCts()}));
+                            }
+                        }
+                    }
+                }
+            },  
+         
             cycleCommand: {
                 command: 'cycle',
                 rank: 'manager',
