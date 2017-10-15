94d528238c5385393a24d0ade2c6eaf64205550b
stiefandawidow <30799966+stiefandawidow@users.noreply.github.com>
2017-10-16 00:42:48 +0200

    Update Catharsis-Bot.js

--- Catharsis-Bot.js
+++ Catharsis-Bot.js
@@ -2593,14 +2593,17 @@
                 command: 'emotes',
                 rank: 'user',
                 type: 'exact',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                functionality: function(chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void(0);
                     else {
-                        return API.sendChat(subChat(basicBot.chat.emotes));
+                        if (typeof basicBot.settings.emotesLink === 'string')
+                            API.sendChat(subChat(basicBot.chat.emotes, {
+                                link: basicBot.settings.emotesLink
+                            }));
                     }
                 }
-            },         
+            },       
          
             fbCommand: {
                 command: 'fb',
